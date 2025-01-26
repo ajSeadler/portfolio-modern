9f23d265bff9bd4787bef11220b7124922b96551
ajSeadler <aj6287@gmail.com>
2025-01-26 14:44:45 -0600

    add side scroll progress ba

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
 import { Navbar } from "@/components/navbar";
+import ScrollProgressBar from "@/components/ScrollProgressBar"; // Import the scroll progress bar
 
 export default function DefaultLayout({
   children,
@@ -8,13 +9,15 @@ export default function DefaultLayout({
 }) {
   return (
     <div className="flex flex-col min-h-screen">
+      {/* Scroll progress bar positioned globally */}
+      <ScrollProgressBar />
       {/* Navbar positioned statically (scrolls with the page) */}
       <Navbar />
       {/* Main content area grows if content exceeds the viewport */}
       <main className="container mx-auto max-w-7xl px-6 flex-grow pt-10">
         {children}
       </main>
-      <div className=" bottom-0">
+      <div className="bottom-0">
         <Footer />
       </div>
     </div>
